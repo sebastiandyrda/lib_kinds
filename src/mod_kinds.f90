1c748017d455b04814764291ee226623ea9609e2
Sebatian Dyrda <sebastian.dyrda@utoronto.ca>
2021-12-31 14:25:00 -0500

    first commit

---
+++ src/mod_kinds.f90
@@ -0,0 +1,10 @@
+module mod_kinds
+
+    use iso_fortran_env, only: real64,int32
+
+    private
+
+    integer,parameter,public :: dp = real64  !! default real kind
+    integer,parameter,public :: ip = int32   !! default integer kind
+
+end module mod_kinds
